2ee17ca636501521efb2c650a0b76750052692ee
ben <ben.uzh@proton.me>
2025-05-11 12:37:19 +0200

    revert include, should use m4

--- envs/build_singularity.sh
+++ envs/build_singularity.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 CMD=singularity
 BUILD=build --fakeroot
-$CMD $BUILD sklearn-optimized.sif sklearn_singularity.def
+$CMD $BUILD sklearn_optimized.sif sklearn_singularity_optimized.def
 $CMD $BUILD clustbench-optimized.sif clustbench_singularity.def
 $CMD $BUILD r.sif r_singularity.def
 $CMD $BUILD fcps.sif fcps_singularity.def
