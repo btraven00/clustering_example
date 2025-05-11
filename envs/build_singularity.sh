e8e0f7eb2313696f7494e65c20d44def8301de63
ben <ben.uzh@proton.me>
2025-05-11 13:29:28 +0200

    escape

--- envs/build_singularity.sh
+++ envs/build_singularity.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 CMD=singularity
-BUILD=build --fakeroot
-$CMD $BUILD clustbench-vanilla.sif clustbench_apptainer_vanillapy.def
-$CMD $BUILD clustbench-optimized.sif clustbench_apptainer_optimized.def
-$CMD $BUILD fcps.sif fcps_singularity_optimized.def
+BUILD='build --fakeroot'
+$CMD ${BUILD} clustbench-vanilla.sif clustbench_apptainer_vanillapy.def
+$CMD ${BUILD} clustbench-optimized.sif clustbench_apptainer_optimized.def
+$CMD ${BUILD} fcps.sif fcps_singularity_optimized.def
