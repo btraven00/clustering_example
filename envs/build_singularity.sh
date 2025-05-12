3890cb48664570aa7a9878dacb299146d69ced5d
ben <ben.uzh@proton.me>
2025-05-12 16:15:38 +0200

    add apptainer definition for rmarkdown

--- envs/build_singularity.sh
+++ envs/build_singularity.sh
@@ -2,5 +2,7 @@
 CMD=singularity
 BUILD='build --fakeroot'
 $CMD ${BUILD} clustbench-vanilla.sif clustbench_apptainer_vanillapy.def
-$CMD ${BUILD} clustbench-optimized.sif clustbench_apptainer_optimized.def
+# enable this if you want to compare with the custom python compilation
+# $CMD ${BUILD} clustbench-optimized.sif clustbench_apptainer_optimized.def
 $CMD ${BUILD} fcps.sif fcps_singularity_optimized.def
+$CMD ${BUILD} rmarkdown.sif rmarkdown.def
