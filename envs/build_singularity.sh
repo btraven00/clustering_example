ebd69b79937e55a68e968d829910c5c6f3d80b70
ben <ben.uzh@proton.me>
2025-05-12 16:38:13 +0200

    cleanup r/fcps deps

--- envs/build_singularity.sh
+++ envs/build_singularity.sh
@@ -4,5 +4,5 @@ BUILD='build --fakeroot'
 $CMD ${BUILD} clustbench-vanilla.sif clustbench_apptainer_vanillapy.def
 # enable this if you want to compare with the custom python compilation
 # $CMD ${BUILD} clustbench-optimized.sif clustbench_apptainer_optimized.def
-$CMD ${BUILD} fcps.sif fcps_singularity_optimized.def
+$CMD ${BUILD} fcps.sif fcps.def
 $CMD ${BUILD} rmarkdown.sif rmarkdown.def
