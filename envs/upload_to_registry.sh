7c8e8b30a1da17c7d8b2bc91d234594141713dbf
ben <ben.uzh@proton.me>
2025-06-23 11:28:54 +0200

    bump apptainer with denet

--- envs/upload_to_registry.sh
+++ envs/upload_to_registry.sh
@@ -3,10 +3,10 @@ USER=user
 REGISTRY=quay.io
 ORGANIZATION=omnibenchmark
 CLUSTBENCH_REPO=clustbench-vanilla
-CLUSTBENCH_TAG=0.1.0
+CLUSTBENCH_TAG=0.1.2
 FCPS_REPO=fcps
-FCPS_TAG=0.1.1
+FCPS_TAG=0.1.2
 
 #singularity registry login --username {$USER} docker://${REGISTRY}
-#singularity push ${CLUSTBENCH_REPO}.sif oras://${REGISTRY}/${ORGANIZATION}/${CLUSTBENCH_REPO}:${CLUSTBENCH_TAG}
+singularity push ${CLUSTBENCH_REPO}.sif oras://${REGISTRY}/${ORGANIZATION}/${CLUSTBENCH_REPO}:${CLUSTBENCH_TAG}
 singularity push ${FCPS_REPO}.sif oras://${REGISTRY}/${ORGANIZATION}/${FCPS_REPO}:${FCPS_TAG}
